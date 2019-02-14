58678adcdef8d6c932cd76dc2365cb960982a578
faboulaws <faboulaws@gmail.com>
2019-02-14 17:58:17 +0200

    added unseed support

--- packages/super-seed-core/lib/BaseDataSource.js
+++ packages/super-seed-core/lib/BaseDataSource.js
@@ -2,4 +2,8 @@ module.exports = class BaseDataSource {
     async createSeeds(seeds) { 
         throw new Error('Unimplemented Method');
     }
+
+    async deleteSeeds() {
+      throw new Error('Unimplemented Method');
+    }
 }

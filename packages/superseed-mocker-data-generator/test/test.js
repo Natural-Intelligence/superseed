d95d925027cceba9a9850123b86a1e90e56f369e
faboulaws <faboulaws@gmail.com>
2019-06-30 13:19:55 +0300

    added test to mongoose mcok generator

---
+++ packages/superseed-mocker-data-generator/test/test.js
@@ -0,0 +1,62 @@
+const {MockDataGeneratorSchema} = require('../index');
+const {expect} = require('chai');
+
+var user = {
+  firstName: {
+    faker: 'name.firstName'
+  },
+  lastName: {
+    faker: 'name.lastName'
+  },
+  country: {
+    faker: 'address.country'
+  },
+  createdAt: {
+    faker: 'date.past'
+  },
+  username: {
+    function: function () {
+      return (
+        this.object.lastName.substring(0, 5) +
+        this.object.firstName.substring(0, 3) +
+        Math.floor(Math.random() * 10)
+      )
+    }
+  }
+};
+
+var group = {
+  description: {
+    faker: 'lorem.paragraph'
+  },
+  users: [
+    {
+      function: function () {
+        return this.faker.random.arrayElement(this.db.user).username
+      },
+      length: 10,
+      fixedLength: false
+    }
+  ]
+};
+
+describe('MockDataGeneratorSchema test', () => {
+  const userGenerator = new MockDataGeneratorSchema('user', user);
+  const groupGenerator = new MockDataGeneratorSchema('group', group);
+  it('relation ship test', () => {
+    const data = userGenerator.generate({}, 1);
+    expect(data.length).to.eql(1);
+    data.forEach(item => {
+      expect(item).to.have.property('firstName');
+      expect(item).to.have.property('lastName');
+    });
+
+    const groupData = groupGenerator.generate({user: data}, 2);
+    groupData.forEach(item => {
+      expect(item).to.have.property('users');
+      expect(item.users).to.include.members([data[0].username]);
+      expect(item).to.have.property('description');
+    });
+  });
+
+});

c5e891daf433fdaf638a5c4be73d0319425d08a3
faboulaws <faboulaws@gmail.com>
2019-07-10 08:03:28 +0300

    updated version to 2.0.0

--- packages/superseed/test/seeding-test.js
+++ packages/superseed/test/seeding-test.js
@@ -6,7 +6,7 @@ const {Seeder, SeedJob} = require('../');
 const {BaseMockGenerator, BaseDataSource} = require('../../superseed-core');
 
 class MyGenerator extends BaseMockGenerator {
-  generateMock(db ,fields) {
+  generateMock(db, fields) {
     return {
       name: fields.name || 'test'
     };
@@ -22,45 +22,45 @@ class MyDataSource extends BaseDataSource {
   }
 }
 
+describe('Seeding tests', () => {
+  describe('SeedJob', () => {
+    const job = new SeedJob('entities', new MyGenerator(), new MyDataSource());
 
-describe('SeedJob', () => {
-  const job = new SeedJob('entities', new MyGenerator(), new MyDataSource());
-
-  it('must generate using count', async () => {
-    const seeder = new Seeder();
-    seeder.addJob(job, {count: 10});
-    const result = await seeder.seed();
-    expect(result.entities.length).to.eql(10)
-  });
-
-  it('must generate using staticFieldData', async () => {
-    const seeder = new Seeder();
-    seeder.addJob(job, {staticFieldData: [{name: "Koffi"}, {name: "Moshe"}, {name: "Ahmed"}]});
-    const result = await seeder.seed();
-    expect(result.entities.length).to.eql(3);
-    ['Koffi','Moshe','Ahmed'].forEach((name,index) => {
-      expect(result.entities[index].name).to.eql(name);
+    it('must generate using count', async () => {
+      const seeder = new Seeder();
+      seeder.addJob(job, {count: 10});
+      const result = await seeder.seed();
+      expect(result.entities.length).to.eql(10)
     });
-  });
 
-  it('must generate using getStaticFieldData', async () => {
-    const seeder = new Seeder();
-    seeder.addJob(job, {getStaticFieldData: () => [{name: "Koffi"}, {name: "Moshe"}, {name: "Ahmed"}]});
-    const result = await seeder.seed();
-    expect(result.entities.length).to.eql(3);
-    ['Koffi','Moshe','Ahmed'].forEach((name,index) => {
-      expect(result.entities[index].name).to.eql(name);
+    it('must generate using staticFieldData', async () => {
+      const seeder = new Seeder();
+      seeder.addJob(job, {staticFieldData: [{name: "Koffi"}, {name: "Moshe"}, {name: "Ahmed"}]});
+      const result = await seeder.seed();
+      expect(result.entities.length).to.eql(3);
+      ['Koffi', 'Moshe', 'Ahmed'].forEach((name, index) => {
+        expect(result.entities[index].name).to.eql(name);
+      });
     });
-  });
 
+    it('must generate using getStaticFieldData', async () => {
+      const seeder = new Seeder();
+      seeder.addJob(job, {getStaticFieldData: () => [{name: "Koffi"}, {name: "Moshe"}, {name: "Ahmed"}]});
+      const result = await seeder.seed();
+      expect(result.entities.length).to.eql(3);
+      ['Koffi', 'Moshe', 'Ahmed'].forEach((name, index) => {
+        expect(result.entities[index].name).to.eql(name);
+      });
+    });
 
-  it('must generate using getStaticFields', async () => {
-    const seeder = new Seeder();
-    seeder.addJob(job, {count: 5, getStaticFields:() => ({name: 'static name'})});
-    const result = await seeder.seed();
-    expect(result.entities.length).to.eql(5);
-    result.entities.forEach((entity) => {
-      expect(entity.name).to.eql('static name');
+    it('must generate using getStaticFields', async () => {
+      const seeder = new Seeder();
+      seeder.addJob(job, {count: 5, getStaticFields: () => ({name: 'static name'})});
+      const result = await seeder.seed();
+      expect(result.entities.length).to.eql(5);
+      result.entities.forEach((entity) => {
+        expect(entity.name).to.eql('static name');
+      });
     });
   });
 });
